e033d25ad4b304e7aea6698f8f7100f5b524e9bf
Randy <randy@dax.coffee>
2023-01-05 12:38:59 +0100

    finished comments

---
+++ Module 1 Data Types and Operations/comments/main.py
@@ -0,0 +1,16 @@
+# Do not modify these lines
+__winc_id__ = '63ce21059cf34d3d8ffef497ede7e317'
+__human_name__ = 'comments'
+
+# Add your code after this line
+
+
+# Useless comment to comment
+tmp = 3         # This is an end of line comment
+awesome = True  # Its usefull when it actually means something
+
+""" useless multi-line comment
+Say yes more. Yes opens doors. No closes them. Yes pushes us. No keeps us safe at home. Imagine all the opportunities waiting for a yes.
+Let go of expectation. You’ve done your best to prepare. Now, let go. There’s no one way your life should unfold. Enjoy the journey.
+Welcome diversions. The most rewarding adventures often start with an unexpected detour. Perhaps that distraction will guide you onward.
+"""
